e8913c43adc40a5adf31b0491396b6c444f17336
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-20 14:08:44 +0530

    Add Header class

--- instagram-clone/Front_End/instar-app/src/App.tsx
+++ instagram-clone/Front_End/instar-app/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-       <h1>Hello World</h1>
+       <Header />
     </div>
   );
 }
