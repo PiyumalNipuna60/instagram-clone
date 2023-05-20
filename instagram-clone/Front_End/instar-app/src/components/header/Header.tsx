c5ebd388c55b5f0ef3e91b830c98327e09078fc3
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-20 22:31:26 +0530

    Add Header content

--- instagram-clone/Front_End/instar-app/src/components/header/Header.tsx
+++ instagram-clone/Front_End/instar-app/src/components/header/Header.tsx
@@ -20,12 +20,42 @@ export default class Header extends Component {
           <div className="header__middle">
             <div className="header__options flex hover:bg-gray-100 rounded-md mt-16 mb-3 mr-2 h-12 items-center">
               <HomeIcon fontSize="large" className='hover:w-9 hover:h-9' />
-              <h6 className='items-center text-center m-2 text-base'>Home</h6>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Home</h6>
             </div>
 
             <div className="header__options flex hover:bg-gray-100 rounded-md mt-3 mr-2 h-12 items-center">
               <SearchIcon fontSize="large" className='hover:w-9 hover:h-9'/>
-              <h6 className='items-center text-center m-2 text-base'>Search</h6>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Search</h6>
+            </div>
+
+            <div className="header__options flex hover:bg-gray-100 rounded-md mt-3 mr-2 h-12 items-center">
+              <ExploreOutlinedIcon fontSize="large" className='hover:w-9 hover:h-9'/>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Explore</h6>
+            </div>
+
+            <div className="header__options flex hover:bg-gray-100 rounded-md mt-3 mr-2 h-12 items-center">
+              <VideoLibraryOutlinedIcon fontSize="large" className='hover:w-9 hover:h-9'/>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Reels</h6>
+            </div>
+
+            <div className="header__options flex hover:bg-gray-100 rounded-md mt-3 mr-2 h-12 items-center">
+              <MessageOutlinedIcon fontSize="large" className='hover:w-9 hover:h-9'/>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Massages</h6>
+            </div>
+
+            <div className="header__options flex hover:bg-gray-100 rounded-md mt-3 mr-2 h-12 items-center">
+              <FavoriteBorderOutlinedIcon fontSize="large" className='hover:w-9 hover:h-9'/>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Notification</h6>
+            </div>
+
+            <div className="header__options flex hover:bg-gray-100 rounded-md mt-3 mr-2 h-12 items-center">
+              <AddBoxOutlinedIcon fontSize="large" className='hover:w-9 hover:h-9'/>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Create</h6>
+            </div>
+
+            <div className="header__options flex hover:bg-gray-100 rounded-md mt-3 mr-2 h-12 items-center">
+              <AccountCircleOutlinedIcon fontSize="large" className='hover:w-9 hover:h-9'/>
+              <h6 className='items-center text-center m-2 text-base/4 font-normal'>Profile</h6>
             </div>
           </div>
         </div>
