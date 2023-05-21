bce4fa9280e058b363e76beb2b45fbae5f7dd25e
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-21 09:19:26 +0530

    Add Content

--- instagram-clone/Front_End/instar-app/src/App.tsx
+++ instagram-clone/Front_End/instar-app/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header/Header';
+import Home from './pages/home/Home';
 
 
 function App() {
   return (
     <div className="App pl-7">
      <Header />
+     <Home />
     </div>
   );
 }
